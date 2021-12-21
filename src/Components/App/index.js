6f2b71f4bc3e04a42aa08d676ea6bc3ef8b8164a
romainAccoce <romainaccoce@gmail.com>
2021-12-21 17:24:12 +0100

    add home componant and sass for style

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,10 @@
-import './App.css';
+import './app.scss';
+import Home from '../Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <Home />
     </div>
   );
 }
---
+++ src/Components/Home/index.js
@@ -0,0 +1,11 @@
+import './home.scss';
+
+const Home = () => {
+    return (
+        <p>home</p>
+    );
+};
+
+export default Home;
+
+
