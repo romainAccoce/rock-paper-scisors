2aa839e842ed1508cc6e27a1bb932e1e3b9cfef9
romainAccoce <romainaccoce@gmail.com>
2021-12-22 13:51:45 +0100

    add header style

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,10 +1,12 @@
 import './app.scss';
 import Home from '../Home';
+import Header from '../Header';
 
 function App() {
   return (
     <div className="App">
-        <Home />
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,7 +3,11 @@ import './header.scss';
 const Header = () => {
     return (
         <div className='header'>
-            <h1 className='header__title'>rock paper scissors</h1>
+            <h1 className='header__title'>
+                rock <br />
+                paper <br />
+                scissors
+            </h1>
             <div className='header__score-container'>
                 <h2 className='header__score-container__title'>score</h2>
                 <div className='header__score-container__score'>12</div>
