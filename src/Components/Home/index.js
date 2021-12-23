80cd7c562cec697ee90a9939cffd3b03e98a5a10
romainAccoce <romainaccoce@gmail.com>
2021-12-23 22:24:22 +0100

    add sign icones

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,34 @@
 import './home.scss';
-import background from '../../assets/images/bg-triangle.svg'
+import background from '../../assets/images/bg-triangle.svg';
+import rockIcon from '../../assets/images/icon-rock.svg';
+import paperIcon from '../../assets/images/icon-paper.svg';
+import scissorsIcon from '../../assets/images/icon-scissors.svg';
 
 const Home = () => {
-    const signes = ['rock', 'paper', 'scissors'];
+
+    const signes = [
+        {
+            name: 'paper',
+            icon: paperIcon,
+        },
+        {
+            name: 'rock',
+            icon: rockIcon,
+        },
+        {
+            name: 'scissors',
+            icon: scissorsIcon,
+        },
+    ];
+
     return (
         <div className='home-container'>
             <img className="home-container__background" src={background} alt='triangle'/>
             {
               signes.map(
                 (signe) => (
-                  <div className="home-container__signe" key={signe}>
-                   {signe}
+                  <div className={`home-container__signe home-container__signe--${signe.name}`} key={signe.name}>
+                   <img className='home-container__signe__image' src={signe.icon}/>
                   </div>
                 ),
                 )
