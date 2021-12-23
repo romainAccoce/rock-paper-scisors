be83edc85e7da3d42514fbc1fe22e27cf92a8be9
romainAccoce <romainaccoce@gmail.com>
2021-12-23 19:45:48 +0100

    add background

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,21 @@
 import './home.scss';
-import Header from '../Header';
+import background from '../../assets/images/bg-triangle.svg'
 
 const Home = () => {
+    const signes = ['rock', 'paper', 'scissors'];
     return (
-        <p>home</p>
+        <div className='home-container'>
+            <img className="home-container__background" src={background} alt='triangle'/>
+            {
+              signes.map(
+                (signe) => (
+                  <div className="home-container__signe" key={signe}>
+                   {signe}
+                  </div>
+                ),
+                )
+            }
+        </div>
     );
 };
 
