8c5b4a1670f8992486858a08508496a544254cb2
romainAccoce <romainaccoce@gmail.com>
2022-01-13 16:05:16 +0100

    fixed redux dev tool and prepare game reducer and actions

---
+++ src/action/game.js
@@ -0,0 +1,15 @@
+export const SELECT_ICON = 'SELECT_ICON';
+export const selectIcon = (value) => (
+  {
+    type: SELECT_ICON,
+    value,
+  }
+);
+
+export const COMPUTER_PLAY = 'COMPUTER_PLAY';
+export const computerPlay = (value) => (
+    {
+        type: COMPUTER_PLAY,
+        value,
+    }
+);
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,8 +1,18 @@
-const initialState = {}
+import { SELECT_ICON, COMPUTER_PLAY } from "../action/game";
+
+const initialState = {
+  playerChoice: '',
+  computerChoice: '',
+  score: 0,
+};
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    
+      case  SELECT_ICON:
+      return {
+        ...state,
+        playerChoice: action.value,
+      }
     default:
       return state;
   }
