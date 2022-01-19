152368648def7f9fe3eee2eee8106f02f409f1fe
romainAccoce <romainaccoce@gmail.com>
2022-01-19 19:15:17 +0100

    css corrections

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,22 +3,23 @@ import './home.scss';
 import Sign from '../Sign';
 import { signs } from '../../selectors/game';
 
+import triangleImage from '../../assets/images/bg-triangle.svg'
+
 const Home = () => {
 
     return (
         <div className='home'>
-            <div className='home__icons'>
-                {   
-                    signs.map(
-                        (sign) => (
-                            <div className={`home__icons__sign--${sign.name}`}>
-                                <Sign sign={sign} key={sign.name}/>
-                            </div>
-                        ),
-                        )
-                    }
+            <img className='home__background' src={triangleImage} />
+            {   
+                signs.map(
+                    (sign) => (
+                        <div className={`home__sign home__sign--${sign.name}`}>
+                            <Sign sign={sign} key={sign.name}/>
+                        </div>
+                    ),
+                    )
+                }
             </div>
-        </div>
     );
 };
 
