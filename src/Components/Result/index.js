7787f44378f461775efbb45827e2c80e12d35dfa
romainAccoce <romainaccoce@gmail.com>
2022-01-20 12:08:58 +0100

    add result sign containers

--- src/Components/Result/index.js
+++ src/Components/Result/index.js
@@ -1,15 +1,22 @@
 import './result.scss';
 
 import Sign from '../Sign';
-import { signs } from '../../selectors/game';
 
 import { findPlayerSign } from '../../selectors/game';
 
 const Result = ({playerChoice}) => {
     return (
-        <div className={`home__icons__sign--${playerChoice}`}>
-            <Sign sign={findPlayerSign(playerChoice)} />
+        <div className='result'>
+            <div className="result__sign-container">
+                <p className="result__sign-container__text">you picked</p>
+                <Sign className="result__sign-container__icon" sign={findPlayerSign(playerChoice)} />
+            </div>
+            <div className="result__sign-container">
+                <p className="result__sign-container__text">the house picked</p>
+                <Sign className="result__sign-container__icon" sign={findPlayerSign(playerChoice)} />
+            </div>
         </div>
+
     );
 };
 
