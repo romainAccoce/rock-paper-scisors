b8067561facc067903eb9fd349e861720ba20959
romainAccoce <romainaccoce@gmail.com>
2022-01-24 16:08:39 +0100

    css optimisations

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,14 +2,15 @@ import './header.scss';
 import { useSelector } from 'react-redux';
 
 import logo from '../../assets/images/logo.svg';
+import logoHardMode from '../../assets/images/logo-bonus.svg'
 
 const Header = () => {
-    
-    const { score } = useSelector((state) => state.game);
+
+    const { score, hardMode } = useSelector((state) => state.game);
 
     return (
         <div className='header'>
-            <img className='header__logo' alt="rock paper scissors logo" src={logo} />
+            <img className='header__logo' alt="rock paper scissors logo" src={hardMode ? logoHardMode : logo} />
 
             <div className='header__score-container'>
                 <h2 className='header__score-container__title'>score</h2>
